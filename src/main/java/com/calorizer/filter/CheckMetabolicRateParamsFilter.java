2435a57d7dbfac8d0f17ac29e59b1ee04d17b592
Anna Manakina <annamanakina@gmail.com>
2019-01-27 16:39:26 +0200

    filter checking the params were added

---
+++ src/main/java/com/calorizer/filter/CheckMetabolicRateParamsFilter.java
@@ -0,0 +1,42 @@
+package com.calorizer.filter;
+
+import com.calorizer.utils.Constant;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/checkMetabolicRateRaramsFilter"})
+public class CheckMetabolicRateParamsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setCharacterEncoding("UTF-8");
+
+        String height = servletRequest.getParameter(Constant.PERSON_HEIGHT);
+        String age = servletRequest.getParameter(Constant.PERSON_AGE);
+        String weight = servletRequest.getParameter(Constant.PERSON_WEIGHT);
+
+        if (age == null & height == null & weight == null ) {
+            sendErrorMessage(servletRequest, servletResponse);
+        } else if ( (age != null && age.equals(""))  &  (height != null && height.equals("")) & (weight != null &&weight.equals(""))){
+            sendErrorMessage(servletRequest, servletResponse);
+        }
+        else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    private void sendErrorMessage(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException {
+        servletRequest.setAttribute("error_message", "заполните необходимые поля!!");
+        servletRequest.getRequestDispatcher("/pages/main.jsp").forward(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- src/main/java/com/calorizer/servlets/UserSettingsServlet.java
+++ src/main/java/com/calorizer/servlets/UserSettingsServlet.java
@@ -1,6 +1,7 @@
 package com.calorizer.servlets;
 
 import com.calorizer.calculation.ActivityMultiplier;
+import com.calorizer.db.PersonDAO;
 import com.calorizer.factory.MetabolicIndexFactory;
 import com.calorizer.items.MetabolicRate;
 import com.calorizer.items.Person;
@@ -16,23 +17,28 @@ import java.io.IOException;
 import static com.calorizer.factory.MetabolicIndexFactory.*;
 
 
-@WebServlet(urlPatterns = {"/usersettings"} )
+@WebServlet(urlPatterns = {"/usersettings"})
 public class UserSettingsServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void init() throws ServletException {
+        System.out.println("init UserSettingsServlet ");
+    }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet UserSettingsServlet ");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
 
-       /* System.out.println("UserSettingsServlet doPost gender: " + request.getParameter("gender")
+        System.out.println("UserSettingsServlet doPost gender: " + request.getParameter("gender")
                 + ",  age: " + request.getParameter("age")
                 + ", height: " + request.getParameter("height")
                 + ", weight: " + request.getParameter("weight")
-                + ", lifestyle: " + request.getParameter("lifestyle"));*/
+                + ", lifestyle: " + request.getParameter("lifestyle"));
 
         String gender = request.getParameter("gender");
         Sex sex = Sex.getValueBy(gender);
@@ -42,25 +48,25 @@ public class UserSettingsServlet extends HttpServlet {
         int weight = Integer.parseInt(request.getParameter("weight"));
         String lifestyle = request.getParameter("lifestyle");
 
-        System.out.println("lifestyle: "+lifestyle);
+        System.out.println("lifestyle: " + lifestyle);
 
         Person person = new Person(sex, age, weight, height);
-
         double bmi = MetabolicIndexFactory.calculate(BMI, person);
         double bmr = MetabolicIndexFactory.calculate(BMR, person);
 
-        //System.out.println("mbi: "+bmi + ", bmr: " +bmr);
         MetabolicRate mr = new MetabolicRate(ActivityMultiplier.getElementBy(lifestyle), bmi, bmr);
-
         person.setMetabolicRate(mr);
-        System.out.println("person: "+person.getMetabolicRate() );
+        //System.out.println("person: "+person.getMetabolicRate() );
 
         double dci = MetabolicIndexFactory.calculate(DAILY_CALORIE_INTAKE, person);
         mr.setDailyCaloriesIntakes(dci);
-        System.out.println("person: "+person.getMetabolicRate() );
-       request.setAttribute("mbi", bmi);
-       request.setAttribute("daily_intake", dci);
-       request.setAttribute("bmr", bmr);
+        person.notifyObservers();
+        //todo add if (user is logged in)
+
+        System.out.println("create person: " + new PersonDAO().create(person));
+        request.setAttribute("mbi", bmi);
+        request.setAttribute("daily_intake", dci);
+        request.setAttribute("bmr", bmr);
         request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
     }
 }
